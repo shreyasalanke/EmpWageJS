66c91dcd0a78dae66a89afe22771f7730cda83c8
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-07 19:36:46 +0530

    ADDED: program to check emp is present or absent

--- EmpWage.js
+++ EmpWage.js
@@ -1,1 +1,9 @@
 console.log("Welcome to The Employee Wage JS Program ") 
+const IS_ABSENT = 0
+
+let empCheck = Math.floor(Math.random()*10) % 2
+if (empCheck == IS_ABSENT){
+    console.log("Employee is Absent")
+} else {
+    console.log("Employee is Present")
+}

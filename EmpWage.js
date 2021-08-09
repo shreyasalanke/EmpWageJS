10a6509532449ff0cdd8e77cb1914d48ac3b0946
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-09 10:53:00 +0530

    ADDED: program to calculate the emp wage

--- EmpWage.js
+++ EmpWage.js
@@ -1,9 +1,22 @@
-console.log("Welcome to The Employee Wage JS Program ") 
-const IS_ABSENT = 0
+console.log("Welcome to Employee Wage JS Program")
 
-let empCheck = Math.floor(Math.random()*10) % 2
-if (empCheck == IS_ABSENT){
-    console.log("Employee is Absent")
-} else {
-    console.log("Employee is Present")
+const IS_PART_TIME = 1
+const IS_FULL_TIME = 2
+const PART_TIME_HOURS = 4
+const FULL_TIME_HOURS = 8
+const WAGE_PER_HOUR =20
+
+let empHours = 0
+let empCheck = Math.floor(Math.random()*10) % 3
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHours =PART_TIME_HOURS
+        break;
+    case IS_FULL_TIME:
+        empHours =FULL_TIME_HOURS
+        break;
+    default:
+        empHours = 0
 }
+let empWage =empHours * WAGE_PER_HOUR
+console.log("Employee Wage "+empWage);

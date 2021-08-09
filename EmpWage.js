03d73d2b95842ea56f879c619fbaed718d955b82
shreyasalanke <shreyasalanke97@gmail.com>
2021-08-09 10:55:02 +0530

    ADDED: program to calculate the emp working hours

--- EmpWage.js
+++ EmpWage.js
@@ -1,22 +1,23 @@
 console.log("Welcome to Employee Wage JS Program")
 
-const IS_PART_TIME = 1
-const IS_FULL_TIME = 2
-const PART_TIME_HOURS = 4
-const FULL_TIME_HOURS = 8
-const WAGE_PER_HOUR =20
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGEE_PER_HOUR=20;
 
-let empHours = 0
-let empCheck = Math.floor(Math.random()*10) % 3
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHours =PART_TIME_HOURS
-        break;
-    case IS_FULL_TIME:
-        empHours =FULL_TIME_HOURS
-        break;
-    default:
-        empHours = 0
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;       
+    }
 }
-let empWage =empHours * WAGE_PER_HOUR
-console.log("Employee Wage "+empWage);
+
+let empCheck=Math.floor(Math.random()*10)%3;
+let empHrs=getWorkingHours(empCheck);
+let empWage=empHrs*WAGEE_PER_HOUR;
+console.log("Hours: "+empHrs+" Emp Wage: "+empWage);
